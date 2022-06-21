7aca18f3d628fd98fdb13e4d22688068dbac10ae
tadgem <98434757+tadgem@users.noreply.github.com>
2022-06-21 23:17:01 +0100

    whitespace changes

--- harmony/include/Rendering/Pipelines/DebugDrawPipeline.h
+++ harmony/include/Rendering/Pipelines/DebugDrawPipeline.h
@@ -21,22 +21,17 @@ namespace harmony
     class DebugDrawStage : public PipelineStage
     {
     public:
-
         DebugDrawStage();
 
         virtual void Init(entt::registry& registry, WeakRef<View> view) override;
         virtual void PreUpdate(entt::registry& registry, WeakRef<View> view) override;
         virtual void PostUpdate(entt::registry& registry, WeakRef<View> view) override;
         virtual void Cleanup() override;
-
-
     };
 
     class DebugDrawPipeline : public Pipeline
     {
     public:
         DebugDrawPipeline();
-
-
     };
 };
