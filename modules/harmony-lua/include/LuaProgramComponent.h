c36bf932a43bf82daf56892c53a251f76528ba35
tadgem <98434757+tadgem@users.noreply.github.com>
2022-06-25 01:56:03 +0100

    add stubs for lua scripting

---
+++ modules/harmony-lua/include/LuaProgramComponent.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Core/ProgramComponent.h"
+#include "sol/sol.hpp"
+namespace harmony
+{
+    class LuaProgramComponent : public ProgramComponent
+    {
+    public:
+        virtual void Init() override;
+        virtual void Update() override;
+        virtual void Render() override;
+        virtual void Cleanup() override;
+        virtual nlohmann::json ToJson() override;
+        virtual void FromJson(const nlohmann::json& json) override;
+    };
+}
---
+++ modules/harmony-lua/src/LuaProgramComponent.cpp
@@ -0,0 +1,26 @@
+#include "LuaProgramComponent.h"
+
+void harmony::LuaProgramComponent::Init()
+{
+}
+
+void harmony::LuaProgramComponent::Update()
+{
+}
+
+void harmony::LuaProgramComponent::Render()
+{
+}
+
+void harmony::LuaProgramComponent::Cleanup()
+{
+}
+
+nlohmann::json harmony::LuaProgramComponent::ToJson()
+{
+    return nlohmann::json();
+}
+
+void harmony::LuaProgramComponent::FromJson(const nlohmann::json& json)
+{
+}
