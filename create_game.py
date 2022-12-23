ba8de076bcf7fdd1e14902867788d259269da40f
tadgem <98434757+tadgem@users.noreply.github.com>
2022-12-23 23:00:45 +0000

    interim commit on runtime refactor
    still need to rename files from templates directory

--- create_game.py
+++ create_game.py
@@ -2,27 +2,34 @@ import os
 import sys
 from distutils.dir_util import copy_tree
 
-template_core_target_name = "game-core-runtime";
-template_editor_target_name = "game-editor-runtime";
+template_core_runtime_target_name = "game-core-runtime";
+template_editor_runtime_target_name = "harmony-editor-runtime";
+template_app_target_name = "harmony-app";
+template_editor_target_name = "harmony-dojo";
+
 
 template_core_includes_name = "GAME_CORE_RUNTIME_INCLUDES"
 template_editor_includes_name = "GAME_EDITOR_INCLUDES"
 
+temp_include_cmake_string = "get_filename_component(TEMP_DIR $\{CMAKE_CURRENT_SOURCE_DIR\} DIRECTORY)"
+
+
 def main():
-    print("Hello World!")
     name = sys.argv[1]
+    print(f"Harmony : Generating project : {name}")
 
     core_runtime_dst = name.lower() + "-core-runtime"
     editor_runtime_dst = name.lower() + "-editor-runtime"
+    app_dst = name.lower() + "-app"
+    editor_dst = name.lower() + "-dojo"
 
-    copy_tree("templates/game-core-runtime", core_runtime_dst)
-    copy_tree("templates/game-editor-runtime", editor_runtime_dst)
+    copy_tree(f"templates/{template_core_runtime_target_name}", core_runtime_dst)
+    copy_tree(f"templates/{template_editor_runtime_target_name}", editor_runtime_dst)
+    copy_tree(f"templates/{template_app_target_name}", app_dst)
+    copy_tree(f"templates/{template_editor_target_name}", editor_dst)
 
-    os.rename(core_runtime_dst + "/include/Game.h", core_runtime_dst + "/include/" + name + ".h")
-    os.rename(core_runtime_dst + "/src/Game.cpp", core_runtime_dst + "/src/" + name + ".cpp")
-
-    os.rename(editor_runtime_dst + "/include/GameEditor.h", editor_runtime_dst + "/include/" + name + "Editor.h")
-    os.rename(editor_runtime_dst + "/src/GameEditor.cpp", editor_runtime_dst + "/src/" + name + "Editor.cpp")
+    os.rename(core_runtime_dst + "/include/GameProgram.h", core_runtime_dst + "/include/" + name + ".h")
+    os.rename(core_runtime_dst + "/src/GameProgram.cpp", core_runtime_dst + "/src/" + name + ".cpp")
 
 if __name__ == "__main__":
     main()
