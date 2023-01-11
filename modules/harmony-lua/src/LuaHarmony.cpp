221d4484e45e6f10eacf4dcb82ef9e254743af70
liam <98434757+tadgem@users.noreply.github.com>
2023-01-11 01:24:50 +0000

    start work on input bindings

--- modules/harmony-lua/src/LuaHarmony.cpp
+++ modules/harmony-lua/src/LuaHarmony.cpp
@@ -133,6 +133,115 @@ void harmony::InitHarmony(sol::state& state)
 {
 	sol::table h = state.create_named_table("harmony");
 
+	h.new_enum("gamePadButton",
+		"FaceNorth", harmony::Gamepad::Button::FaceNorth,
+		"FaceEast", harmony::Gamepad::Button::FaceEast,
+		"FaceSouth", harmony::Gamepad::Button::FaceSouth,
+		"FaceWest", harmony::Gamepad::Button::FaceWest,
+		"Up", harmony::Gamepad::Button::Up,
+		"Right", harmony::Gamepad::Button::Right,
+		"Down", harmony::Gamepad::Button::Down,
+		"Left", harmony::Gamepad::Button::Left,
+		"RB", harmony::Gamepad::Button::RightBumper,
+		"LB", harmony::Gamepad::Button::LeftBumper,
+		"Home", harmony::Gamepad::Button::Home,
+		"Back", harmony::Gamepad::Button::Select,
+		"Start", harmony::Gamepad::Button::Start
+		);
+
+	h.new_enum("gamePadStick",
+		"LS", harmony::Gamepad::Stick::LS,
+		"RS", harmony::Gamepad::Stick::RS
+	);
+
+	h.new_enum("gamePadTrigger",
+		"LT", harmony::Gamepad::Trigger::LT,
+		"RT", harmony::Gamepad::Trigger::RT
+	);
+
+	h.new_enum("mouseButton",
+		"Left", harmony::Mouse::Button::Left,
+		"Right", harmony::Mouse::Button::Right,
+		"Middle", harmony::Mouse::Button::Middle
+	);
+
+	h.new_enum("key",
+		"A", A,
+		"B", B,
+		"C", C,
+		"D", D,
+		"E", E,
+		"F", F,
+		"G", G,
+		"H", H,
+		"I", I,
+		"J", J,
+		"K", K,
+		"L", L,
+		"M", M,
+		"N", N,
+		"O", O,
+		"P", P,
+		"Q", Q,
+		"R", R,
+		"S", S,
+		"T", T,
+		"U", U,
+		"V", V,
+		"W", W,
+		"X", X,
+		"Y", Y,
+		"Z", Z,
+		"Zero", Zero,
+		"One", One,
+		"Two", Two,
+		"Three", Three,
+		"Four", Four,
+		"Five", Five,
+		"Six", Six,
+		"Seven", Seven,
+		"Eight", Eight,
+		"Nine", Nine,
+		"Minus", Minus,
+		"Underscore", Underscore,
+		"Equals", Equals,
+		"Plus", Plus,
+		"Backspace", Backspace,
+		"Enter", Enter,
+		"Space", Space,
+		"Tab", Tab,
+		"CapsLock", CapsLock,
+		"Tilde", Tilde,
+		"LeftShift", LeftShift,
+		"LeftControl", LeftControl,
+		"LeftAlt", LeftAlt,
+		"RightShift", RightShift,
+		"RightControl", RightControl,
+		"RightAlt", RightAlt,
+		"F1", F1,
+		"F2", F2,
+		"F3", F3,
+		"F4", F4,
+		"F5", F5,
+		"F6", F6,
+		"F7", F7,
+		"F8", F8,
+		"F9", F9,
+		"F10", F10,
+		"F11", F11,
+		"F12", F12,
+		"Insert", Insert,
+		"Home", Home,
+		"PageUp", PageUp,
+		"PageDown", PageDown,
+		"Delete", Delete,
+		"End", End,
+		"Up", Up,
+		"Down", Down,
+		"Left", Left,
+		"Right", Right
+		);
+
 	// High level
 	auto entityHandleDef	= h.new_usertype<entt::entity>("entity_handle");
 	auto entityDef		= h.new_usertype<harmony::Entity>("entity", "handle", &harmony::Entity::m_Handle);
@@ -143,6 +252,8 @@ void harmony::InitHarmony(sol::state& state)
 	h.set_function("OpenScene", lua_OpenScene);
 	h.set_function("GetScene", lua_GetActiveScene);
 	h.set_function("GetEntity", lua_GetEntity);
+
+	h.set_function("Input.GetKey", harmony::Input::GetGamepadButton);
 
 	sceneDef["AddEntity"] = lua_AddEntity;
 
