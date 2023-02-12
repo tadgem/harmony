ed7ac82a90d2b6550af7e0dd0b48c7f21c062bf0
tadgem <98434757+tadgem@users.noreply.github.com>
2023-02-12 15:12:50 +0000

    more setup for mono

---
+++ create_mono_project.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+from sdl2 import *
+import ctypes
+import OpenGL.GL as gl
+import shutil
+import os
+
+import imgui
+from imgui.integrations.sdl2 import SDL2Renderer
+
+sample_project_path = "sandbox/projects/sample"
+
+def create_project_window(path_text_val, name_text_val):
+    imgui.text("Create Project Tool")
+    changed, path_text = imgui.input_text(
+    'Path',
+    path_text_val,
+    256
+    )
+    changed, name_text = imgui.input_text(
+    'Name',
+    name_text_val,
+    256
+    )
+
+    if imgui.button("Generate New Project"):
+        shutil.copytree(sample_project_path, path_text, symlinks=False, ignore=None, copy_function=shutil.copy2, ignore_dangling_symlinks=False, dirs_exist_ok=False)
+        project_file_path = path_text + "/" + "sample.harmonyproj"
+        project_text = ""
+        with open(project_file_path, "r") as file:
+            project_text = file.read()
+            project_text = project_text.replace("D:/repos/harmony/sandbox/projects/sample", path_text )
+            project_text = project_text.replace("sample", name_text)
+            file.close()
+        with open(project_file_path, "w") as file:
+            file.write(project_text)
+            file.close()
+        
+        os.rename(project_file_path, path_text + "/" + name_text + ".harmonyproj")
+        os.rename(project_file_path + ".ini", path_text + "/" + name_text  + ".harmonyproj" + ".ini")
+        os.rename(path_text + "/" + "sample.das", path_text + "/" + name_text + ".das")
+    return (path_text, name_text)
+
+
+def main():
+    window, gl_context = impl_pysdl2_init()
+    imgui.create_context()
+    impl = SDL2Renderer(window)
+
+    path_text_val = 'd:/harmony-proj/test'
+    name_text_val = 'test'
+
+    running = True
+    event = SDL_Event()
+    while running:
+        while SDL_PollEvent(ctypes.byref(event)) != 0:
+            if event.type == SDL_QUIT:
+                running = False
+                break
+            impl.process_event(event)
+        impl.process_inputs()
+
+        imgui.new_frame()
+
+        if imgui.begin_main_menu_bar():
+            if imgui.begin_menu("File", True):
+
+                clicked_quit, selected_quit = imgui.menu_item(
+                    "Quit", 'Cmd+Q', False, True
+                )
+
+                if clicked_quit:
+                    exit(1)
+
+                imgui.end_menu()
+            imgui.end_main_menu_bar()
+
+
+        imgui.begin("Create Project window", True)
+        path_str, name_str = create_project_window(path_text_val, name_text_val)
+        
+        path_text_val = path_str
+        name_text_val = name_str
+        imgui.end()
+
+        gl.glClearColor(0., 0., 0., 1)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+        imgui.render()
+        impl.render(imgui.get_draw_data())
+        SDL_GL_SwapWindow(window)
+
+    impl.shutdown()
+    SDL_GL_DeleteContext(gl_context)
+    SDL_DestroyWindow(window)
+    SDL_Quit()
+
+
+def impl_pysdl2_init():
+    width, height = 1280, 720
+    window_name = "minimal ImGui/SDL2 example"
+
+    if SDL_Init(SDL_INIT_EVERYTHING) < 0:
+        print("Error: SDL could not initialize! SDL Error: " + SDL_GetError().decode("utf-8"))
+        exit(1)
+
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1)
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24)
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8)
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1)
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1)
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE)
+
+    SDL_SetHint(SDL_HINT_MAC_CTRL_CLICK_EMULATE_RIGHT_CLICK, b"1")
+    SDL_SetHint(SDL_HINT_VIDEO_HIGHDPI_DISABLED, b"1")
+
+    window = SDL_CreateWindow(window_name.encode('utf-8'),
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              width, height,
+                              SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE)
+
+    if window is None:
+        print("Error: Window could not be created! SDL Error: " + SDL_GetError().decode("utf-8"))
+        exit(1)
+
+    gl_context = SDL_GL_CreateContext(window)
+    if gl_context is None:
+        print("Error: Cannot create OpenGL Context! SDL Error: " + SDL_GetError().decode("utf-8"))
+        exit(1)
+
+    SDL_GL_MakeCurrent(window, gl_context)
+    if SDL_GL_SetSwapInterval(1) < 0:
+        print("Warning: Unable to set VSync! SDL Error: " + SDL_GetError().decode("utf-8"))
+        exit(1)
+
+    return window, gl_context
+
+if __name__ == "__main__":
+    main()
---
+++ targets/mono-editor-test/main.cpp
@@ -0,0 +1,23 @@
+#include "Core/Memory.h"
+
+void* operator new(size_t size)
+{
+	harmony::Memory::AddAllocatedMemory(size);
+	return malloc(size);
+}
+
+void operator delete(void* memory, size_t size)
+{
+	harmony::Memory::AddFreedMemory(size);
+	free(memory);
+}
+
+#include "EditorApplication.h"
+
+int main()
+{
+	harmony::Editor app;
+	app.Run();
+
+	return 0;
+}
