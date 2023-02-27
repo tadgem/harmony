807df26d43cb612d632584be0d2b6a4ac11f4b5e
tadgem <98434757+tadgem@users.noreply.github.com>
2023-02-27 20:03:45 +0000

    stubs for C# core

---
+++ projects/mono-test/Harmony-Script/Behaviour.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Harmony
+{
+    public interface Behaviour
+    {
+        public Entity ThisEntity { get; }
+
+        void Init();
+        void Update();
+        void Render();
+        void Cleanup();
+    }
+}
