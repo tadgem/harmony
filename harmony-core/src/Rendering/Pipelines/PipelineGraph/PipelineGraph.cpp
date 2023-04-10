47d604981f5188e1510484db61526453d1320149
tadgem <98434757+tadgem@users.noreply.github.com>
2023-04-10 21:38:31 +0100

    stub pipeline graph api

---
+++ harmony-core/src/Rendering/Pipelines/PipelineGraph/PipelineGraph.cpp
@@ -0,0 +1,13 @@
+#include "Rendering/Pipelines/PipelineGraph/PipelineGraph.h"
+
+harmony::PipelineGraph::PipelineGraph() {
+
+}
+
+harmony::PipelineGraph::~PipelineGraph() {
+
+}
+
+void harmony::PipelineGraph::ProcessGraph(entt::registry &registry, harmony::WeakRef<harmony::View> view) {
+
+}
