c82a372b21cdee112b49a46b29fcdc397743c1e7
tadgem <98434757+tadgem@users.noreply.github.com>
2023-06-11 22:50:53 +0100

    stub entity data component

--- harmony-core/include/ECS/Entity.h
+++ harmony-core/include/ECS/Entity.h
@@ -1,10 +1,22 @@
 #pragma once
-
+#include "Core/Alias.h"
 #include "ThirdParty/entt.hpp"
 #include "ThirdParty/json.hpp"
 #include <optional>
 
 namespace harmony {
+
+    struct EntityData
+    {
+        String m_Name;
+        uint64_t m_LayerMask = 0; // TODO: do something with this.
+        entt::entity m_Parent  {UINT32_MAX};
+        bool m_Static = false;
+
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(EntityData, m_Name, m_LayerMask, m_Parent, m_Static)
+
+    };
+
     class Entity {
     public:
         Entity();
